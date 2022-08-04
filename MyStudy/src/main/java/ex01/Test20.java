ef2d71e6699ec2d3dc61e97b1e44bca422cb9340
cornchiplick <ilsunkiss@gmail.com>
2022-08-04 20:41:47 +0900

    20220804

---
+++ MyStudy/src/main/java/ex01/Test20.java
@@ -0,0 +1,42 @@
+// copyright by codefights.com
+//
+// 양의 정수가 주어질 때 가장 큰 숫자를 찾아라!
+// 예)
+//    maxDigit(2542) ==> 5
+//
+//
+/*
+Find the largest digit of the given number.
+
+[input] integer n
+non-negative integer
+
+[output] integer
+largest digit of n
+ */
+//
+// [시간 복잡도]
+// - ?
+//
+public class Test20 {
+
+  public static void main(String[] args) {
+    System.out.println(maxDigit(736) == 7);
+    System.out.println(maxDigit(2542) == 5);
+  }
+
+  // 이 메서드를 완성하시오!
+  static int maxDigit(int n) {
+    int max = 0;
+
+    while (n != 0) {
+      if ((n % 10) >= max) {
+        max = (n % 10);
+      }
+
+      n /= 10;
+    }
+
+    return max;
+  }
+}
