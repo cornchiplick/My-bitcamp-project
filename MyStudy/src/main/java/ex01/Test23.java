9a86a531088526d5e7ca79798a8ab4132aec3457
cornchiplick <ilsunkiss@gmail.com>
2022-08-09 17:08:46 +0900

    20220809

---
+++ MyStudy/src/main/java/ex01/Test23.java
@@ -0,0 +1,39 @@
+// copyright by codefights.com
+//
+// 문자열에 포함된 숫자의 합을 구하라.
+// 예)
+//   sumUpNumbers("2 apples, 12 oranges") ==> 5
+//
+/*
+Find the sum of all digits that occur in a string.
+
+Example
+
+sumUpNumbers("2 apples, 12 oranges") = 5
+
+[input] string inputString
+
+[output] integer
+ */
+//
+// [시간 복잡도]
+// - ?
+//
+public class Test23 {
+
+  public static void main(String[] args) {
+    System.out.println(sumUpDigits("2 apples, 12 oranges") == 5);
+  }
+
+  // 이 메서드를 완성하시오!
+  static int sumUpDigits(String inputString) {
+    int answer = 0;
+    for (int i = 0; i < inputString.length(); i++) {
+      if (inputString.charAt(i) >= 0) {
+        System.out.println(inputString.charAt(i));
+        answer += inputString.charAt(i) - '0';
+      }
+    }
+    return answer;
+  }
+}
