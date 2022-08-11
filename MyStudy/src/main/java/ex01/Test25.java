80a694d723c01e5d4718443cc50bf83d9ce11d13
cornchiplick <ilsunkiss@gmail.com>
2022-08-11 16:36:26 +0900

    20220811

---
+++ MyStudy/src/main/java/ex01/Test25.java
@@ -0,0 +1,50 @@
+// copyright by codefights.com
+//
+// 정수의 자릿수가 주어질 때,
+// 앞자리가 0이 아닌 가장 작은 정수의 값을 구하라.
+//
+// 예)
+//   smallestNumber(1) ==> 0
+//   smallestNumber(2) ==> 10
+//   smallestNumber(3) ==> 100
+//
+//
+/*
+The algorithm should return the smallest non-negative integer of N digits length.
+
+Example
+
+smallestNumber(2) = 10
+
+[input] integer n
+integer's length, positive integer
+
+[output] integer
+ */
+//
+// [시간 복잡도]
+// - O(n): n은 자릿수
+//
+public class Test25 {
+
+  public static void main(String[] args) {
+    System.out.println(smallestNumber(1) == 0);
+    System.out.println(smallestNumber(2) == 10);
+    System.out.println(smallestNumber(3) == 100);
+    System.out.println(smallestNumber(4) == 1000);
+  }
+
+  // 이 메서드를 완성하시오!
+  static int smallestNumber(int n) {
+    if (n == 1) {
+      return 0;
+    }
+
+    int result = 1;
+    for (int i = 2; i <= n; i++) {
+      result *= 10;
+    }
+
+    return result;
+  }
+}
