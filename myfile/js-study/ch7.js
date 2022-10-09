4d660e6508b379383465db5d46adc26a1aa1ce17
cornchiplick <ilsunkiss@gmail.com>
2022-10-09 22:17:45 +0900

    add: javascript ch.7 study

---
+++ myfile/js-study/ch7.js
@@ -0,0 +1,15 @@
+let number = new Number(273)
+let num = 485
+
+console.log(number)
+console.log(num)
+
+console.log("-------------------------------------");
+
+let num2 = 273
+console.log(num2.toFixed(2))
+
+console.log("-------------------------------------");
+
+let num3 = Number.NaN
+console.log(num3)
